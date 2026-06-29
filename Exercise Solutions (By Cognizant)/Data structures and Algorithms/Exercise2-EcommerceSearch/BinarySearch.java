4e70a7e65b7737422c0bb7b45fe9481d97df0466
Shivansh Mittal <www.shivansh065@gmail.com>
2026-06-29 15:01:37 +0530

    add DSA exercises: E-commerce Search and Financial Forecasting

---
+++ Exercise Solutions (By Cognizant)/Data structures and Algorithms/Exercise2-EcommerceSearch/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+
+    // needs a sorted array
+    public static Product searchById(Product[] products, int targetId) {
+        int low = 0;
+        int high = products.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (products[mid].productId == targetId) {
+                return products[mid];
+            } else if (products[mid].productId < targetId) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return null;
+    }
+}
